317e7d7c88e428866b3180e880b3b7b8ead1809b
Daniel <info@danielvaladas.com>
2024-02-06 00:08:30 -0500

    validator unit-tests

---
+++ Services/Payment/PaymentRequestValidator.cs
@@ -0,0 +1,24 @@
+﻿// MIT Licensed.
+
+namespace EcomCli.Services.Payment
+{
+    using FluentValidation;
+
+    /// <summary>
+    /// Validates <see cref="PaymentRequest"/>.
+    /// </summary>
+    internal class PaymentRequestValidator : AbstractValidator<PaymentRequest>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PaymentRequestValidator"/> class.
+        /// </summary>
+        public PaymentRequestValidator()
+        {
+            this.RuleFor(x => x.Amount)
+                .GreaterThan(0);
+            this.RuleFor(x => x.CreditCard)
+                .NotEmpty()
+                .CreditCard();
+        }
+    }
+}
---
+++ Tests/UnitTests/Services/Payment/PaymentRequestValidatorTests.cs
@@ -0,0 +1,73 @@
+﻿using EcomCli.Services.Payment;
+using FluentValidation;
+using Xunit;
+
+namespace UnitTests.Services.Payment
+{
+    public class PaymentRequestValidatorTests
+    {
+        private readonly IValidator<PaymentRequest> validator;
+
+        public PaymentRequestValidatorTests()
+        {
+            this.validator = new PaymentRequestValidator();
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-1)]
+        [InlineData(-1000)]
+        [InlineData(-0.01)]
+        [InlineData(-0.00001)]
+        public void RequiresPositiveAmount(decimal amount)
+        {
+            // Arrange
+            var request = new PaymentRequest(amount, "1234123412341234");
+
+            // Act
+            var result = this.validator.Validate(request);
+
+            // Assert
+            Assert.False(result.IsValid);
+            Assert.Contains(result.Errors, x => x.PropertyName == "Amount");
+        }
+
+        [Theory]
+        [InlineData("1234123412341234")]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("1234")]
+        [InlineData(" ")]
+        [InlineData("abc")]
+        public void Refuses_Invalid_CreditCard(string creditCard)
+        {
+            // Arrange
+            var request = new PaymentRequest(123.45m, creditCard);
+
+            // Act
+            var result = this.validator.Validate(request);
+
+            // Assert
+            Assert.False(result.IsValid);
+            Assert.Contains(result.Errors, x => x.PropertyName == "CreditCard");
+        }
+
+        [Theory]
+        [InlineData(1.23, "4111111111111111")] // Visa valid test card
+        [InlineData(2.34, "4111-1111-1111-1111")] // Should work with dashes too
+        [InlineData(3.45, "4111 1111 1111 1111")] // Spaces should work too
+        [InlineData(123.00, "5555555555554444")] // MasterCard valid test card
+        [InlineData(1999.99, "378282246310005")] // American Express valid test card
+        public void ValidPayment_PassesValidation(decimal amount, string creditCard)
+        {
+            // Arrange
+            var request = new PaymentRequest(amount, creditCard);
+
+            // Act
+            var result = this.validator.Validate(request);
+
+            // Assert
+            Assert.True(result.IsValid);
+        }
+    }
+}
