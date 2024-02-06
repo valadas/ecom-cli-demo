8b43272c21f3841bd5c4e60be8d43b9222550192
Daniel <info@danielvaladas.com>
2024-02-05 22:56:07 -0500

    Basic unit tests

---
+++ Services/Payment/IPaymentService.cs
@@ -0,0 +1,17 @@
+﻿// MIT Licensed.
+
+namespace EcomCli.Services.Payment
+{
+    /// <summary>
+    /// Provides payment related services.
+    /// </summary>
+    internal interface IPaymentService
+    {
+        /// <summary>
+        /// Processes the payment.
+        /// </summary>
+        /// <param name="request">The details about the payment.</param>
+        /// <returns>True if the payment succeeded, false otherwhise.</returns>
+        bool ProcessPayment(PaymentRequest request);
+    }
+}
